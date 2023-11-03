1611991c473b2b105c87d9fe45cceadcf5ad363f
Jan-Emig <jan.emig@pm.me>
2023-11-03 17:29:36 +0100

    feat(employees): Add overview chart

--- app/(internal)/dash/components/tabs/employees/EmployeesTab.tsx
+++ app/(internal)/dash/components/tabs/employees/EmployeesTab.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 
 import TrendCards from '../TrendCards';
 import TbMoodUp from '@/components/icons/TbMoodUp';
@@ -6,8 +6,15 @@ import { IIconProps } from '@/components/icons/types/icons';
 import TbCalendarStats from '@/components/icons/TbCalendarStats';
 import TbArrowsExchange2 from '@/components/icons/TbArrowsExchange2';
 import TbHeartHandshake from '@/components/icons/TbHeartHandshake';
-import ActivityGrid from '@/components/ui/activity-grid';
-import { Card } from '@/components/ui/card';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Area, AreaChart, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { Skeleton } from '@/components/ui/skeleton';
+
+type TChartData = {
+  name: string;
+  office: number;
+  remote: number;
+};
 
 const emptyCardItems: Array<{
   title: string;
@@ -61,7 +68,7 @@ const loadedCardItems: Array<{
     title: 'Total Employees',
     icon: TbHeartHandshake,
     content: {
-      amount: (Math.random() * 7500).toFixed(2),
+      amount: (Math.random() * 7500).toFixed(0),
       comparison: {
         value: Math.random() * 100 * (Math.random() > 0.2 ? 1 : -1),
         label: '% from last month'
@@ -108,30 +115,134 @@ const loadedCardItems: Array<{
  */
 const EmployeesTab: FC = () => {
   const [trendData, setTrendData] = useState(emptyCardItems);
+  const [chartData, setChartData] = useState<TChartData[]>();
 
   useEffect(() => {
     setTimeout(() => {
       setTrendData(loadedCardItems);
+      setChartData([
+        {
+          name: 'Jan',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Feb',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Mar',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Apr',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'May',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Jun',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Jul',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Aug',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Sep',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Oct',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Nov',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        },
+        {
+          name: 'Dec',
+          office: Math.random() * 500,
+          remote: Math.random() * 500
+        }
+      ]);
     }, 2000);
   }, []);
+
+  let chart: ReactNode = null;
+  if (chartData) {
+    if (chartData.length > 0) {
+      chart = (
+        <div className="-ml-7">
+          <ResponsiveContainer width="100%" height={350}>
+            <AreaChart data={chartData}>
+              <defs>
+                <linearGradient id="colorOffice" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#1d4ed8" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#1d4ed8" stopOpacity={0} />
+                </linearGradient>
+                <linearGradient id="colorRemote" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#f97316" stopOpacity={0.8} />
+                  <stop offset="95%" stopColor="#f97316" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis dataKey="name" axisLine={false} tickLine={false} fontSize={12} />
+              <YAxis axisLine={false} tickLine={false} fontSize={12} />
+              <Area
+                type="monotone"
+                dataKey="office"
+                stroke="#1d4ed8"
+                fillOpacity={1}
+                fill="url(#colorOffice)"
+              />
+              <Area
+                type="monotone"
+                dataKey="remote"
+                stroke="#f97316"
+                fillOpacity={1}
+                fill="url(#colorRemote)"
+              />
+              <Legend />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+      );
+    } else {
+      chart = (
+        <div className="flex flex-1 justify-center items-center h-full text-muted-foreground text-sm">
+          - No data available -
+        </div>
+      );
+    }
+  } else {
+    chart = <Skeleton className="w-full h-full" />;
+  }
 
   return (
     <div className="flex flex-col h-full gap-5">
       <TrendCards data={trendData} />
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 mb-10 h-auto overflow-hidden">
-        <Card className="h-[220px]">
-          <ActivityGrid
-            data={{
-              items: Array.from({ length: 30 }, (_, i) => ({
-                date: new Date(new Date().setDate(i + 1)),
-                value: Math.floor(Math.random() * 100)
-              })),
-              endDate: new Date('2023-11-14'),
-              max: 9999,
-              min: 0,
-              startDate: new Date('2023-11-01')
-            }}
-          />
+        <Card className="flex flex-col h-[440px]">
+          <CardHeader>
+            <h3 className="font-semibold leading-none tracking-tight">Workplace Presence</h3>
+          </CardHeader>
+          <CardContent className="flex-1 p-6 pt-0">{chart}</CardContent>
         </Card>
       </div>
     </div>
--- app/(internal)/dash/components/tabs/overview/OverviewTab.tsx
+++ app/(internal)/dash/components/tabs/overview/OverviewTab.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from 'next-themes';
-import { FC, useEffect, useState } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 import { Bar, BarChart, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
@@ -11,81 +11,14 @@ import TbCash from '@/components/icons/TbCash';
 import TbMoodUp from '@/components/icons/TbMoodUp';
 import { IIconProps } from '@/components/icons/types/icons';
 import { TTrendCard } from '../../../types/trends-card';
+import { Skeleton } from '@/components/ui/skeleton';
 
-const cashFlowData: Array<{ name: string; ocf: number; icf: number; fcf: number }> = [
-  {
-    name: 'Jan',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Feb',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Mar',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Apr',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'May',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Jun',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Jul',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Aug',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Sep',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Oct',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Nov',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  },
-  {
-    name: 'Dec',
-    ocf: Math.random() * 10000,
-    icf: Math.random() * 10000,
-    fcf: Math.random() * 10000
-  }
-];
+type TChartData = {
+  name: string;
+  ocf: number;
+  icf: number;
+  fcf: number;
+};
 
 const emptyCardItems: Array<{
   title: string;
@@ -190,58 +123,149 @@ const loadedCardItems: Array<{
  */
 const OverviewTab: FC = () => {
   const [trendData, setTrendData] = useState<TTrendCard[]>(emptyCardItems);
+  const [chartData, setChartData] = useState<TChartData[]>();
   const { theme } = useTheme();
 
   useEffect(() => {
     setTimeout(() => {
       setTrendData(loadedCardItems);
+      setChartData([
+        {
+          name: 'Jan',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Feb',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Mar',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Apr',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'May',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Jun',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Jul',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Aug',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Sep',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Oct',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Nov',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        },
+        {
+          name: 'Dec',
+          ocf: Math.random() * 10000,
+          icf: Math.random() * 10000,
+          fcf: Math.random() * 10000
+        }
+      ]);
     }, 2000);
   }, []);
 
-  const chartFcfColor =
-    theme === 'dark'
-      ? { ocf: '#1d4ed8', icf: '#3b82f6', fcf: '#93c5fd' }
-      : { ocf: '#1e40af', icf: '#2563eb', fcf: '#3b82f6' };
+  let chart: ReactNode = null;
+  if (chartData) {
+    if (chartData.length > 0) {
+      const chartFcfColor =
+        theme === 'dark'
+          ? { ocf: '#1d4ed8', icf: '#3b82f6', fcf: '#93c5fd' }
+          : { ocf: '#1e40af', icf: '#2563eb', fcf: '#3b82f6' };
+      chart = (
+        <div className="-ml-2">
+          <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={chartData}>
+              <XAxis dataKey="name" axisLine={false} tickLine={false} fontSize={12} />
+              <YAxis axisLine={false} tickLine={false} fontSize={12} unit="$" />
+              <Legend />
+              <Bar
+                name="Operating"
+                dataKey="ocf"
+                stackId="a"
+                fill={chartFcfColor.ocf}
+                className="fill-blue-700 dark:fill-blue-800"
+              />
+              <Bar
+                name="Investing"
+                dataKey="icf"
+                stackId="a"
+                fill={chartFcfColor.icf}
+                className="fill-blue-500 dark:fill-blue-600"
+              />
+              <Bar
+                name="Financial"
+                dataKey="fcf"
+                stackId="a"
+                fill={chartFcfColor.fcf}
+                className="fill-blue-400 dark:fill-blue-500"
+                radius={[5, 5, 0, 0]}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      );
+    } else {
+      chart = (
+        <div className="flex flex-1 justify-center items-center h-full text-muted-foreground text-sm">
+          - No data available -
+        </div>
+      );
+    }
+  } else {
+    chart = <Skeleton className="w-full h-full" />;
+  }
 
   return (
     <div className="flex flex-col h-full gap-5">
       <TrendCards data={trendData} />
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 mb-10 h-auto overflow-hidden">
-        <Card className="h-[440px]">
-          <CardHeader className="flex flex-col space-y-1.5 p-6">
+        <Card className="flex flex-col h-[440px]">
+          <CardHeader>
             <h3 className="font-semibold leading-none tracking-tight">Cash Flow</h3>
           </CardHeader>
-          <CardContent className="p-6 pt-0 pl-2">
-            <ResponsiveContainer width="100%" height={350}>
-              <BarChart data={cashFlowData}>
-                <XAxis dataKey="name" axisLine={false} tickLine={false} fontSize={12} />
-                <YAxis axisLine={false} tickLine={false} fontSize={12} unit="$" />
-                <Legend />
-                <Bar
-                  name="Operating"
-                  dataKey="ocf"
-                  stackId="a"
-                  fill={chartFcfColor.ocf}
-                  className="fill-blue-700 dark:fill-blue-800"
-                />
-                <Bar
-                  name="Investing"
-                  dataKey="icf"
-                  stackId="a"
-                  fill={chartFcfColor.icf}
-                  className="fill-blue-500 dark:fill-blue-600"
-                />
-                <Bar
-                  name="Financial"
-                  dataKey="fcf"
-                  stackId="a"
-                  fill={chartFcfColor.fcf}
-                  className="fill-blue-400 dark:fill-blue-500"
-                  radius={[5, 5, 0, 0]}
-                />
-              </BarChart>
-            </ResponsiveContainer>
-          </CardContent>
+          <CardContent className="flex-1 p-6 pt-0">{chart}</CardContent>
         </Card>
         <ProjectsOverviewCard />
       </div>
