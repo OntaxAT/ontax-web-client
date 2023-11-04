519fc571d1a79b5b6256d16bb306704377fef676
Jan-Emig <jan.emig@pm.me>
2023-11-04 23:42:46 +0100

    feat(dash): Improved welcome message

--- app/(internal)/dash/components/DashboardWelcome.tsx
+++ app/(internal)/dash/components/DashboardWelcome.tsx
@@ -1,33 +1,43 @@
 import { currentUser } from '@/lib/constants/user';
-import { FC, memo, useEffect, useState } from 'react';
+import { FC, ReactNode, memo, useEffect, useState } from 'react';
+
+const greetings = [
+  'Welcome back',
+  'Hello',
+  'Good to see you',
+  "Glad you're here",
+  'Nice to see you',
+  'Welcome'
+];
 
 const messages: string[] = [
   // Positive messages
-  'Welcome back, $u! The business is flourishing, check the financial and projects tab for more information.',
-  'Hello, $u! The recent product launch was a success, check the projects tab for more information.',
-  'Good to see you, $u! Customer feedback has been overwhelmingly positive, check the notifications tab for more information.',
-  "Glad you're here, $u! Projects have been running smoothly, check the projects tab to track their progress.",
-  "Nice to see you, $u! The team's collaborative efforts have been paying off, leading to strong project outcomes.",
+  'The business is flourishing, check the financial and projects tab for more information.',
+  'The recent product launch was a success, check the projects tab for more information.',
+  'Customer feedback has been overwhelmingly positive, check the notifications tab for more information.',
+  'Projects have been running smoothly, check the projects tab to track their progress.',
+  "The team's collaborative efforts have been paying off, leading to strong project outcomes.",
   // Negativ messages
-  'Welcome back, $u! The company is facing some challenges, check the financial reports and the employees tab for more information.',
-  'Hello, $u! The recent product launch is slightly behind the expectations, check the projects tab for more information.',
-  "Good to see you, $u! Some customer feedback wasn't mostly positive, check the notifications tab for more information.",
-  "Glad you're here, $u! Projects have faced some hiccups, check the projects tab for more information.",
-  'Nice to see you, $u! Customer satisfaction has dipped somewhat, check the clients area for more information.'
+  'The company is facing some challenges, check the financial reports and the employees tab for more information.',
+  'The recent product launch is slightly behind the expectations, check the projects tab for more information.',
+  "Some customer feedback wasn't positive, check the notifications tab for more information.",
+  'Projects have faced some hiccups, check the projects tab for more information.',
+  'Customer satisfaction has dipped somewhat, check the clients area for more information.'
 ];
 
 /**
  * Dynamic welcome message on the dashboard (memoized)
  */
 const DashboardWelcome: FC = () => {
-  const [msg, setMsg] = useState<string>();
+  const [msg, setMsg] = useState<ReactNode>();
 
   useEffect(() => {
+    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
     setMsg(
-      messages[Math.floor(Math.random() * messages.length)].replace(
-        '$u',
-        currentUser.details.firstName
-      )
+      <>
+        {greeting},&nbsp;<span className="font-bold">{currentUser.details.firstName}</span>!&nbsp;
+        {messages[Math.floor(Math.random() * messages.length)]}
+      </>
     );
   }, []);
 
